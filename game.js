69c21742e53c117f140e3b24a8ed0022e183f428
DanielToft92 <toftgaming92@gmail.com>
2025-01-27 20:56:40 +0100

    game.js fil added

---
+++ game.js
@@ -0,0 +1,14 @@
+
+xPos = 400;
+yPos = 0;
+
+function setup() {
+    createCanvas(800, 800);
+}
+
+function draw() {
+    background(220);
+    noStroke();
+    fill(0, 0, 255);
+    circle(xPos, yPos, 100);
+}
